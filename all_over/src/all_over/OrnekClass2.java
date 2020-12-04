6527c3bd9720285072f1fa0d0701e9b73a31ee19
furkan-PC <furkan-PC@furkan>
2020-12-04 17:01:21 +0300

    04.12.2020

---
+++ all_over/src/all_over/OrnekClass2.java
@@ -0,0 +1,17 @@
+package all_over;
+
+public class OrnekClass2 {
+
+	public static void main(String[] args) {
+		
+		methodProgramlama(123.5);
+	}
+	public static void methodProgramlama(double puan)
+	{
+		if(puan >= 202.5) System.out.println("puan: a1");
+		else if(puan >= 122.4) System.out.println("puan: a2");
+		else System.out.println("puan: a3");
+	}
+	
+
+}
