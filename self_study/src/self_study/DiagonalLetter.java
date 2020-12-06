c9ae6adfe666ee6ebbef3f7a7a9dd6ea8b44e351
furkan-PC <furkan-PC@furkan>
2020-12-06 19:34:19 +0300

    diagonal letter placement with the for loop

--- self_study/src/self_study/DiagonalLetter.java
+++ self_study/src/self_study/DiagonalLetter.java
@@ -5,19 +5,20 @@ public class DiagonalLetter {
 
 	public static void main(String[] args) {
 		
-		Scanner keyboard = new Scanner(System.in);
-		System.out.print("diagonal siralanmasi icin harf gir > ");
-		String key = keyboard.next();
-		
-		int i;
-		final int LIMIT = 10;
-		String a = " ";
-		for(i = 0; i < LIMIT; i++)
-		{
-			for(int j = 0; j < i; j++) {
-				System.out.print(a);
+		try (Scanner keyboard = new Scanner(System.in)) {
+			System.out.print("diagonal siralanmasi icin harf gir > ");
+			String key = keyboard.next();
+			
+			int i;
+			final int LIMIT = 10;
+			String a = " ";
+			for(i = 0; i < LIMIT; i++)
+			{
+				for(int j = 0; j < i; j++) {
+					System.out.print(a);
+				}
+				System.out.println(key);
 			}
-			System.out.println(key);
 		}
 	}
 }
