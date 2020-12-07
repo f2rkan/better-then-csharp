379d5e00c0f9e14e79adb7cbc78478f5c26cd207
furkan-PC <furkan-PC@furkan>
2020-12-07 16:24:14 +0300

    learning about the character using the character class

---
+++ self_study/src/self_study/CharacterInfo.java
@@ -0,0 +1,26 @@
+package self_study;
+
+public class CharacterInfo {
+
+	public static void main(String[] args) {
+		
+		char aChar = 'C';
+		System.out.println(aChar + " karakteri;");
+		
+		if(Character.isUpperCase(aChar)) System.out.println(aChar + ", buyuk harftir.");
+		else System.out.println(aChar + ", buyuk harf degildir.");
+		
+		if(Character.isLowerCase(aChar)) System.out.println(aChar + ", kucuk harftir.");
+		else System.out.println(aChar + ", kucuk harf degildir");
+		
+		aChar = Character.toLowerCase(aChar);
+		System.out.println("toLowerCase() sonrasi, aChar > " + aChar);
+		
+		aChar = Character.toUpperCase(aChar);
+		System.out.println("toUpperCase() sonrasi, aChar > " + aChar);
+		
+		
+		
+	}
+
+}
