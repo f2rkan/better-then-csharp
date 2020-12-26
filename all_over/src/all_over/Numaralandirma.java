6e53514d6e5e1e05518406da7f24d9a9f0995975
furkan-PC <furkan-PC@furkan>
2020-12-26 19:54:22 +0300

    enum example

--- all_over/src/all_over/Numaralandirma.java
+++ all_over/src/all_over/Numaralandirma.java
@@ -1,5 +1,12 @@
 package all_over;
 
-public enum Numaralandirma {
+public enum Numaralandirma{
 
+	//Deneme1 = 5, Deneme2, Deneme3;
+	//Deger atamalarinda parantez kullanýlmalý
+	deneme1(3), deneme2(4), deneme3(5);
+
+	Numaralandirma(int i) {
+		// TODO Auto-generated constructor stub
+	}
 }
